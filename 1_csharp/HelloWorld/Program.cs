d93489a179344320651a7a943f6d43c2a36a7795
Andrew Agatep <andrew.agatep04gmail.com>
2020-02-26 11:26:21 -0600

    add csharp directory

---
+++ 1_csharp/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Digimon{
+      
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World");
+        }
+
+        public static void Fight(Digimon a, Digimon b){
+          System.Console.WriteLine("hehe");
+        }
+    }
+}
---
+++ 1_csharp/Test1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Test1
+{
+    public class Program1
+    {
+      protected internal int test = 5;
+        
+    }
+
+    class Program2:Program1
+    {
+      public void print(){
+        System.Console.WriteLine(test);
+      }
+      
+    }
+
+    class Test
+    {
+      static void Main(string[] args)
+      {
+        Program1 p = new Program1();
+        //p.print();
+        System.Console.WriteLine(p.test);
+      }
+    }
+}
